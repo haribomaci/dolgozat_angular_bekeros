e6cc0e55b931ac195e50d77b44db44b3b3cc0178
toth julia <t.julia982@gmail.com>
2022-11-23 16:52:18 +0100

    kész dolgozat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'telteg';
+  a :number = 0;
+  b :number = 0;
+  area !: number;
+  areaShow = false;
+
+  
+  calcHandle():void{
+    if(this.checkValidation()){
+      console.log("működik")
+      this.area = this.calcArea(this.a, this.b);
+      this.a = 0;
+      this.b = 0;
+      this.areaShow = true;
+    }else{
+      alert("Hiba! Érvénytelen bemenő adatok");
+    }
+
+}
+
+    calcArea(a: number, b: number):number {
+      let area = a * b;
+      return area;
+
+    }
+    checkValidation():boolean{
+      let valid = true
+      if(this.a <= 0){
+        valid = false;
+
+      }
+      if(this.b <= 0){
+        valid = false;
+
+      }
+      return valid;
+    }
+    hideArea():void{
+      this.areaShow = false;
+    }
 }
